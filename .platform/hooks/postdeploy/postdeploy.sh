f90722fca99a9d2ef3f9e05d871c255d41938c86
Fryxis <lennycoston@outlook.fr>
2024-10-29 13:02:27 +0100

    ajout droit

--- .platform/hooks/postdeploy/postdeploy.sh
+++ .platform/hooks/postdeploy/postdeploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Postdeploy script"
 
-
 mkdir -p /var/current/storage/framework/sessions
 mkdir -p /var/current/storage/framework/views
 mkdir -p /var/current/storage/framework/cache
@@ -15,3 +14,17 @@ chmod -R 777 /var/current/storage/logs
 chmod -R 777 /var/current/ressources/lang
 chmod -R 777 /var/current/bootstrap/cache
 chown -R root:root /var/current/storage
+
+mkdir -p /var/www/storage/framework/sessions
+mkdir -p /var/www/storage/framework/views
+mkdir -p /var/www/storage/framework/cache
+mkdir -p /var/www/storage/logs
+chmod -R 777 /var/www/storage 
+chmod -R 777 /var/www/storage/framework
+chmod -R 777 /var/www/storage/framework/sessions
+chmod -R 777 /var/www/storage/framework/views
+chmod -R 777 /var/www/storage/framework/cache
+chmod -R 777 /var/www/storage/logs
+chmod -R 777 /var/www/ressources/lang
+chmod -R 777 /var/www/bootstrap/cache
+chown -R root:root /var/www/storage
