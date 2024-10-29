aeddccc481bfa40ee8fbd5a9d97eaec6e66c4eba
Fryxis <lennycoston@outlook.fr>
2024-10-29 14:18:12 +0100

    correction pwd

--- .platform/hooks/postdeploy/postdeploy.sh
+++ .platform/hooks/postdeploy/postdeploy.sh
@@ -11,8 +11,8 @@ chmod -R 777 /var/current/storage/framework/sessions
 chmod -R 777 /var/current/storage/framework/views
 chmod -R 777 /var/current/storage/framework/cache
 chmod -R 777 /var/current/storage/logs
-chmod -R 777 /var/current/ressources/lang
-chmod -R 777 /var/current/bootstrap/cache
+chmod -R 777 /var/current/resources/lang/
+chmod -R 777 /var/current/bootstrap/cache/
 chown -R root:root /var/current/storage
 
 mkdir -p /var/www/html/storage/framework/sessions
@@ -25,8 +25,8 @@ chmod -R 777 /var/www/html/storage/framework/sessions
 chmod -R 777 /var/www/html/storage/framework/views
 chmod -R 777 /var/www/html/storage/framework/cache
 chmod -R 777 /var/www/html/storage/logs
-chmod -R 777 /var/www/html/ressources/lang/
+chmod -R 777 /var/www/html/resources/lang/
 chmod -R 777 /var/www/html/bootstrap/cache/
 chown -R root:root /var/www/html/storage
 cd /var/www/html/
-php artisan /var/www/html/storage:link
+rm -f public/storage && php artisan storage:link
